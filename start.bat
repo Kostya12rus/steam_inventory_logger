8349a83b2c17051dadb3355cf8e4bb0462cf06af
Kostya <mkg97@yandex.ru>
2024-07-05 00:01:36 +0300

    start bat

---
+++ start.bat
@@ -0,0 +1,21 @@
+@echo off
+REM Определяем путь к проекту относительно текущего местоположения батника
+set PROJECT_DIR=%~dp0
+
+REM Переходим в директорию с Python
+cd /d "C:\Program Files\Python311"
+
+REM Обновляем pip
+python.exe -m pip install --upgrade pip
+
+REM Переходим в директорию проекта
+cd /d "%PROJECT_DIR%"
+
+REM Устанавливаем зависимости
+python.exe -m pip install -r requirements.txt
+
+REM Запускаем основной скрипт
+python.exe main_flet.py
+
+REM Ожидаем нажатия клавиши для завершения
+pause
